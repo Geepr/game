6136225cb70ba4564178d2613fade3f97fb3f4c0
Piotr Kowalski <piotr@pkowalski.net>
2023-08-27 13:39:58 +0200

    Allow `TitleOverride` of `GameRelease` to be null in model

--- models/game_release.go
+++ models/game_release.go
@@ -9,7 +9,7 @@ type GameRelease struct {
 	Id     uuid.UUID
 	GameId uuid.UUID
 	// TitleOverride can be used if the title of the game is somehow modified for a specific release.
-	TitleOverride string
+	TitleOverride *string
 	Description   *string
 	// ReleaseDate indicates when this release was (or will be) published. Nil if not known.
 	// Keep in mind that setting nil assumes that this is already released.
