1ede9629cb09eef949585d5428fa0d4897ee0330
Piotr Kowalski <piotr@pkowalski.net>
2023-08-31 20:57:40 +0200

    Add basic logging support

--- main.go
+++ main.go
@@ -4,7 +4,16 @@ import (
 	"github.com/Geepr/game/database"
 	"github.com/KowalskiPiotr98/gotabase"
 	_ "github.com/lib/pq"
+	log "github.com/sirupsen/logrus"
+	"os"
 )
+
+func init() {
+	log.SetFormatter(&log.TextFormatter{FullTimestamp: true, DisableColors: true})
+	log.SetOutput(os.Stdout)
+	//todo: configurable
+	log.SetLevel(log.InfoLevel)
+}
 
 func main() {
 	err := gotabase.InitialiseConnection("user=postgres dbname=geepr password=postgres sslmode=disable", "postgres")
