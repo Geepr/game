b78b43c06bbb73bfe93ac4eaa40c6e12562cc6e8
Piotr Kowalski <piotr@pkowalski.net>
2023-10-24 20:27:07 +0200

    Fix game json naming

--- models/game.go
+++ models/game.go
@@ -6,12 +6,12 @@ import (
 
 // Game represents an existing or future game, regardless of play status, platform, release date, etc.
 type Game struct {
-	Id uuid.UUID
+	Id uuid.UUID `json:"id"`
 	// Title is a common name of the game.
 	// In cases when different platforms or releases differ in naming, those changes can be set per-release.
-	Title       string
-	Description *string
+	Title       string  `json:"title"`
+	Description *string `json:"description"`
 	// Archived games are generally hidden from most views, but not removed outright.
 	// This allows users to hide certain titles but keep the data for future reference.
-	Archived bool
+	Archived bool `json:"archived"`
 }
